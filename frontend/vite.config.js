f49a3b1b10785a8d199ec76ebb345c0955cc001e
Anubhav kumar patel <anubhav24@navgurukul.org>
2025-11-10 23:13:30 +0530

    save

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://e-react-ten.vercel.app:5000',
+        target: 'http://e-react-ten.vercel.app:5000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
